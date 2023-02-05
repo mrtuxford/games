e46ded5025fbd58936adf72c20e4a2b2582a419d
mrtuxford <124531186+mrtuxford@users.noreply.github.com>
2023-02-06 07:50:42 +1100

    Initial commit

---
+++ Never Have I Ever Games/neverhaveiever.js
@@ -0,0 +1,270 @@
+const prompts = [
+"eaten food that fell on the floor", 
+"accidentally spelled my name wrong", 
+"slept in regular clothing", 
+"worn my shirt inside out in public by mistake", 
+"pretended to laugh at a joke I didn't get", 
+"watched the Ghostbusters remake", 
+"eaten a bug", 
+"watched SpongeBob SquarePants", 
+"caught someone picking their nose", 
+"eaten what I knew was someone else's food", 
+"made dumb faces at myself in the bathroom mirror", 
+"used a public bathroom", 
+"cried during a Pixar movie", 
+"laughed so hard I wet myself", 
+"dressed as the opposite sex", 
+"accidentally called a teacher Mum", 
+"had a crush", 
+"pulled a push door", 
+"rubbed someone else's toothbrush in something other than toothpaste", 
+"tried to see how many marshmallows can fit into my mouth", 
+"worn my parents' clothes", 
+"“cleaned up” by piling everything into a closet", 
+"been arrested", 
+"hidden food under my bed", 
+"stayed up until three in the morning", 
+"run into a sliding glass door", 
+"sung karaoke", 
+"gone without a bath or shower for more than a week", 
+"done something so bad in public that I still cringe when I think about it", 
+"eaten chalk", 
+"tripped and fallen in public", 
+"dropped my phone in the toilet", 
+"baby talked to someone who wasn't a baby", 
+"watched the 'Gangnam Style' music video", 
+"been electrocuted", 
+"shoplifted", 
+"got high", 
+"made silly faces on my parents and teachers", 
+"dyed my hair and regretted it terribly", 
+"been afraid of a shadow", 
+"pretended to know a stranger", 
+"broken something expensive because I was angry", 
+"used the gents' room in a club because the line for the ladies' room was too long", 
+"been called out for having dandruff", 
+"sung miserably in a bathroom", 
+"worn sleepwear and pretended it was clothing", 
+"spilled a drink on a stranger", 
+"shared my utensils with my dog", 
+"snuck in somewhere", 
+"shaved my eyebrows off", 
+"danced in front of a mirror", 
+"said “excuse me” when there was no one around", 
+"worn my underwear inside out", 
+"scared myself in a mirror", 
+"dyed my bathwater", 
+"partied for more than 12 hours", 
+"practiced a speech in front of the mirror", 
+"pulled out my loose teeth and shown them to others", 
+"missed a high five", 
+"tried out to be an extra in a movie", 
+"sang in the shower", 
+"been chased by a dog", 
+"broken into someone's yard to use their pool", 
+"wondered how it is to be a dog", 
+"gotten a tattoo", 
+"been attacked by a goose", 
+"secretly wished I were a wizard at Hogwarts", 
+"dressed like a monster", 
+"done something impolite because of peer pressure", 
+"cheated on a test", 
+"disliked the opinion of others", 
+"been out of the country", 
+"taken part in a protest", 
+"jumped into a pool with all my clothes on", 
+"eaten raw fish", 
+"used a swear word", 
+"re-gifted a gift card", 
+"smoked", 
+"cut my own hair", 
+"cried in public", 
+"traveled out of state by myself", 
+"fallen asleep in public", 
+"lived alone", 
+"shaved my head", 
+"skipped class", 
+"flown in a helicopter", 
+"lied to my teachers", 
+"watched TV the entire day", 
+"swum with dolphins", 
+"tried sushi", 
+"spoken to my crush", 
+"been on stage in front of a crowd", 
+"lost my wallet", 
+"broken a bone", 
+"been to a concert", 
+"eaten alligator", 
+"stalked a crush", 
+"sang in public", 
+"faked sickness to miss school", 
+"hosted a Halloween party", 
+"had a boyfriend or girlfriend", 
+"thought about what type of dog I would be", 
+"played a musical instrument", 
+"been to Mexico", 
+"shaved my legs", 
+"watched children's cartoons", 
+"gone snowboarding", 
+"unfollowed a friend on social media", 
+"touched a snake", 
+"finished a Sudoku puzzle", 
+"forgotten the sunglasses that I was already wearing", 
+"gone skiing", 
+"given a speech in front of a crowd", 
+"picked up a spider", 
+"lied to a friend to avoid a greater evil", 
+"locked my keys in my car", 
+"traveled to a foreign country", 
+"run a marathon", 
+"tried a recipe from the Internet", 
+"stolen money from my father's wallet", 
+"not tipped at a restaurant", 
+"learned a foreign language", 
+"lifted weights for a workout", 
+"ridden an elephant", 
+"taken alcohol without the consent of my parents", 
+"given money to a homeless person", 
+"played truth or darewent viral online", 
+"worked in a fast-food restaurant", 
+"played spin the bottle", 
+"tried to look at the sun", 
+"texted or taken a call at the movies", 
+"passed out", 
+"bungee-jumped", 
+"donated an organ", 
+"saved someone's life", 
+"donated blood", 
+"met someone famous", 
+"had surgery", 
+"lied to a law enforcement officer", 
+"made a recipe from a blog", 
+"auditioned for a talent show", 
+"bet on something", 
+"jumped out of a plane", 
+"gone vegan", 
+"seen a famous person on the street", 
+"run away from home", 
+"flown in a plane", 
+"made a wish at a fountain", 
+"used a fake ID", 
+"told a lie to my best friend", 
+"been chased by a wild animal", 
+"worn a weird costume to school", 
+"accidentally eaten a bug", 
+"lied about my age", 
+"pulled an all-nighter during school", 
+"cooked", 
+"put candy on pizza", 
+"tried to open someone's phone", 
+"had chickenpox", 
+"made up a story to impress someone", 
+"slept with my pet", 
+"been behind the wheel of a car", 
+"read a single Harry Potter book", 
+"cut in line on purpose", 
+"cheated on a test", 
+"been to the circus", 
+"hidden some important piece of information from the person who should know", 
+"been inside of a library", 
+"blabbed something I swore to secrecy", 
+"unshared my location with a friend", 
+"built a snowman", 
+"given money to a homeless person", 
+"had a cavity", 
+"tried a fad diet", 
+"been on a cruise ship", 
+"watched the TV upside down", 
+"broken a bone playing sports", 
+"seen an elephant in real life", 
+"snooped through a friend's room, cabinet, or property", 
+"made up a story about someone that wasn't real", 
+"kissed my pet", 
+"misplaced a valuable possession of mine", 
+"been to Disney World", 
+"had friends with benefits", 
+"baked my own bread", 
+"ridden a motorbike", 
+"been jealous of my friends", 
+"bought clothing online", 
+"been in a talent show", 
+"thrown a surprise party", 
+"made someone cry by giving them a present", 
+"cried in the school bathroom", 
+"spent hours watching funny videos on YouTube", 
+"gotten stitches", 
+"complained about a roommate", 
+"tried pet food", 
+"been late to class", 
+"thought a movie was better than the book", 
+"been scuba diving", 
+"slept in my roommate's bed", 
+"tried baby food as an adult", 
+"had frostbite", 
+"had food poisoning", 
+"hit a parked car", 
+]
+
+window.addEventListener("load", myInit, true); function myInit(){
+    updateQuestion(0);
+    loadQuestions();
+  }
+
+// Creates question number nav bar with onclick event to update the question for its position in the filtered question list.
+function loadQuestions(){
+  
+    // update navbar items with html
+    question_nav_html = ""
+    for (let i = 0; i < prompts.length; i++) {  
+      // Render the active question
+      var selected_question_number = parseInt(localStorage.getItem('never_have_I_ever_selected_question'));
+      var active_class = ""
+      if (selected_question_number == i){
+        active_class = "active "
+        console.log("here")
+      }
+  
+      question_nav_html += "<li class=\"nav-item rounded-circle " + active_class + "\"><a class=\"nav-link\" onclick=\"updateQuestion(" + i + ")\" href=\"#\">" + i + "</a></li>";
+  
+    }      
+    console.log(question_nav_html);
+    document.getElementById("question_nav").innerHTML = question_nav_html
+}
+
+function updateQuestion(question_index){
+    localStorage.setItem('never_have_I_ever_selected_question', question_index);
+    document.getElementById("question_display").innerHTML = prompts[question_index];
+
+    var selected_question_number = parseInt(localStorage.getItem('never_have_I_ever_selected_question'));
+    // console.log(selected_question_number);
+
+    loadQuestions();
+}
+
+
+function gotoNextQuestion(){
+    var current_question = parseInt(localStorage.getItem('never_have_I_ever_selected_question'))
+    var next_question = (current_question+1) % prompts.length
+    updateQuestion(next_question)
+}
+  
+function gotoPrevQuestion(){
+    var current_question = parseInt(localStorage.getItem('never_have_I_ever_selected_question'))
+    var prev_question = (current_question-1)
+    if (prev_question<0){
+      updateQuestion(prompts.length-1)
+    } else {
+      updateQuestion(prev_question)
+    }
+}
+
+function gotoRandomQuestion(){
+    var current_question = parseInt(localStorage.getItem('never_have_I_ever_selected_question'))
+    var random_index = 0;
+    do {
+        random_index = Math.floor(Math.random() * prompts.length);
+    }
+    while (random_index == current_question);
+
+    updateQuestion(random_index);
+}
---
+++ main.js
@@ -0,0 +1,187 @@
+const word_games_activities = [
+    {
+        category: "word_games",
+        name: "Anagram",
+        link: "Anagram Game/anagram.html",
+        time: "2min+",
+        tags: ["group","individual"],
+        summary: "Unscramble the letters to find the original word in the brain-teasing game of Anagram!",
+    },
+    {
+        category: "word_games",
+        name: "Word Rush",
+        link: "Word Rush Game/wordrush.html",
+        time: "2min+",
+        tags: ["group","individual"],
+        summary: "A fast paced word search game where you come up with as many words as possible that use the letters in a given word.", 
+    },
+    {
+        category: "word_games",
+        name: "Scattergories",
+        link: "https://swellgarfo.com/scattergories/",
+        time: "2min+",
+        tags: ["group","individual"],
+        summary: "Scattergories is a creative-thinking category-based party game.", 
+    },
+    {
+        category: "word_games",
+        name: "Hangman",
+        link: "",
+        time: "2min+",
+        tags: ["group","individual"],
+        summary: "Unleash your inner detective and test your vocabulary skills with the classic game of Hangman!", 
+    },
+    {
+        category: "word_games",
+        name: "Dingbats",
+        link: "",
+        time: "2min+",
+        tags: ["group","individual"],
+        summary: "The game, for two or more people, involves solving rebuses: puzzles in which a common word or saying is hidden in a cryptic or otherwise unique arrangement of symbols.", 
+    },
+    {
+        category: "word_games",
+        name: "Riddles",
+        link: "Riddles Game/riddles.html",
+        time: "2min+",
+        tags: ["group","individual"],
+        summary: "\"What am I?\"", 
+    },
+    {
+        category: "word_games",
+        name: "Spelling Bee",
+        link: "",
+        time: "2min+",
+        tags: ["individual"],
+        summary: "", 
+    }
+]
+
+const math_games_activities = [
+    {
+        category: "maths_games",
+        name: "Greedy Pig",
+        link: "Greedy Pig Game/greedypig.html",
+        time: "5min+",
+        tags: ["activity","individual"],
+        summary: "This game gets people moving around the room and is a bit like musical chairs but less violent!"
+    },
+    {
+        category: "maths_games",
+        name: "Closest Number",
+        link: "Closest Number Game/closestnumber.html",
+        time: "2min+",
+        tags: ["individual"],
+        summary: "Using the numbers provided, how close can you get to a specified number using the basic operations of mathematics?"
+    }
+]
+
+const māori_games_activities = [
+    {
+        category: "māori_games",
+        name: "Rangitahi",
+        link: "",
+        time: "5min+",
+        tags: ["activity","group"],
+        summary: "This game gets people moving around the room and is a bit like musical chairs but less violent!"
+    }
+]
+
+const quizzes_activities = [
+    {
+        category: "quizzes",
+        name: "Kahoot",
+        link: "https://kahoot.com/schools-u/",
+        time: "5min+",
+        tags: ["device","individual"],
+        summary: "Kahoot! is a game-based learning platform that makes it easy to create, share and play learning games or trivia quizzes in minutes."
+    },
+    {
+        category: "quizzes",
+        name: "Blooket",
+        link: "https://www.blooket.com/",
+        time: "5min+",
+        tags: ["device","individual"],
+        summary: "Blooket is a multi-choice trivia platform that engages students with a range of game modes."
+    },
+    {
+        category: "quizzes",
+        name: "Geo Guesser",
+        link: "https://geoguessmaster.net/",
+        time: "2min+",
+        tags: ["group"],
+        summary: "You are placed somewhere is the world and you need to use the clues around you to determine where."
+    },
+    
+]
+
+const team_building_activities = [
+    {
+        category: "team_building",
+        name: "Never Have I Ever",
+        link: "Never Have I Ever Games/neverhaveiever.html",
+        time: "5min+",
+        tags: ["group"],
+        summary: "'Never Have I Ever' is a game of truth-telling and storytelling."
+        }
+]
+
+const other_activities = [
+    {
+        category: "other",
+        name: "Mafia",
+        link: "Mafia Game/mafia.html",
+        time: "10min+",
+        tags: ["activity","group"],
+        summary: "'Mafia' is a game of deception and deduction played in two teams: mafia and townspeople."
+    }
+]
+
+
+const activity_lists = [word_games_activities, math_games_activities, māori_games_activities, quizzes_activities, team_building_activities, other_activities]
+
+window.addEventListener("load", myInit, true); function myInit(){
+    generateActivityList()
+  }
+
+  
+// Dynamically generate the activity lists based on activities object.
+// This will create a list item with the activity title/link, description, and tags
+function generateActivityList(){
+    for (let x in activity_lists){
+        var category_list = activity_lists[x];
+        var activity_html;
+        for (let i=0; i<category_list.length;i++){
+            activity = category_list[i];
+            activity_html = "";
+            activity_html += "<li class=\"list-group-item\">";
+            // Title with link
+            activity_html += "<h1><a href=\"" + activity.link + "\">" + activity.name + "</a></h1>";
+
+            // Time tag HTML
+            activity_html += "<span class=\"badge rounded-pill bg-time\">" + activity.time + "</span>\n";
+
+            // Other tags HTML
+            for (x in activity.tags){
+                tag = activity.tags[x];
+                if (tag == "device"){
+                    activity_html += "<span class=\"badge rounded-pill bg-device\" data-bs-toggle=\"tooltip\" data-bs-placement=\"right\" title=\"Devices Required\"><img src=\"/Anagram Game/res/laptop.svg\" alt=\"Devices\"></span>\n"
+                } else if (tag == "activity") {
+                    activity_html += "<span class=\"badge rounded-pill bg-activity\" data-bs-toggle=\"tooltip\" data-bs-placement=\"right\" title=\"Movement Required\"><img src=\"/Anagram Game/res/activity.svg\" alt=\"Movement\"></span>\n"
+                } else if (tag == "group") {
+                    activity_html += "<span class=\"badge rounded-pill bg-group\" data-bs-toggle=\"tooltip\" data-bs-placement=\"right\" title=\"Groups\"><img src=\"/Anagram Game/res/people.svg\" alt=\"Group\"></span>\n"
+                } else if (tag == "individual") {
+                    activity_html += "<span class=\"badge rounded-pill bg-individual\" data-bs-toggle=\"tooltip\" data-bs-placement=\"right\" title=\"Individual\"><img src=\"/Anagram Game/res/person.svg\" alt=\"Individual\"></span>\n"
+                }
+            }
+            
+            // Summary description
+            activity_html += "<p>" + activity.summary + "</p>";
+            activity_html += "</li>";
+            document.getElementById(activity.category).innerHTML += activity_html;
+        }
+    }
+}
+
+
+
